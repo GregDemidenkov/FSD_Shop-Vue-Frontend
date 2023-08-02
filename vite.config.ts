62da19f0da8e35d5e4ecd9fbe6819d8798d30d41
podlunoy <greg.demidenkov@yandex.ru>
2023-08-02 19:35:27 +0300

    Add dependencies. Setup alias.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src/app'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@entities': path.resolve(__dirname, './src/entities'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  }
 })
